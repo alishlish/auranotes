d9b0709baa4de0911a2d76c0bad88c7df95e4442
Alisha Mishra <alishm1@uci.edu>
2025-09-03 12:21:39 -0700

    added vite, tailwind, js functionality

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-500">
+      <h1 className="text-5xl font-bold text-white">Tailwind works 🎉</h1>
+    </div>
+  )
+}
+
+export default App
